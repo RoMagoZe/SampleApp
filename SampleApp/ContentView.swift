72a9887f4f790b3af0389d891299725e53561a1d
masaharu <dabao456democebu3@gmail.com>
2024-06-30 16:34:23 +0900

    Initial commit

---
+++ SampleApp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SampleApp
+//
+//  Created by Mina on 2024/06/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SampleApp/SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  SampleApp
+//
+//  Created by Mina on 2024/06/29.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
